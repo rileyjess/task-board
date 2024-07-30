6d16e66f14d7f1202fb7c40ff0ea3d185502a601
good morning <172862789+jr-jpg@users.noreply.github.com>
2024-07-30 15:48:05 -0500

    Wrote logic to create a new task card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,3 @@
-// Add datepicker to modal
-$( function() {
-    $( "#taskDueDate" ).datepicker({
-      changeMonth: true,
-      changeYear: true
-    });
-  } );
-
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
@@ -18,9 +10,39 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-    addTaskBtn.on('click', function() {
+    const taskCard = $('<div>')
+     .addClass('card project-card draggable my-3')
+     .attr('data-project-id', project.id);
+    const cardHeader = $('<div>').addClass('card-header h4').text(task.title);
+    const cardBody = $('<div>').addClass('card-body');
+    const cardDescription = $('<p>').addClass('card-text').text(task.description);
+    const cardDueDate = $('<p>').addClass('card-text').text(project.dueDate);
+    const cardDeleteBtn = $('<button>')
+     .addClass('btn btn-danger delete')
+     .text('Delete')
+     .attr('data-project-id', project.id);
+    cardDeleteBtn.on('click', handleDeleteProject);
 
-    })
+  // If the task is not marked as done, background color styles will be applied based on the due date
+  if (task.dueDate && task.status !== 'done') {
+    const now = dayjs();
+    const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+
+    // The card will be yellow if the due date is today. The card will be red if the task is overdue.
+    if (now.isSame(taskDueDate, 'day')) {
+      taskCard.addClass('bg-warning text-white');
+    } else if (now.isAfter(taskDueDate)) {
+      taskCard.addClass('bg-danger text-white');
+      cardDeleteBtn.addClass('border-light');
+    }
+}
+
+// All the elements just created will be appended together
+cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
+taskCard.append(cardHeader, cardBody);
+
+return taskCard;
+
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -46,4 +68,9 @@ function handleDrop(event, ui) {
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
 
+    // Initialize the date picker
+    $( "#taskDueDate" ).datepicker({
+        changeMonth: true,
+        changeYear: true
+      });
 });
